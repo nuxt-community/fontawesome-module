e0a09dd735d9052b0e5a96b421ac80cad52d6977
pimlie <pimlie@hotmail.com>
2020-02-08 17:07:05 +0100

    fix: use unique imports
    test: add test for proIcons

---
+++ test/config-pro.test.js
@@ -0,0 +1,50 @@
+const { resolve } = require('path')
+const fs = require('fs-extra')
+const { Nuxt, Builder } = require('nuxt-edge')
+
+describe('basic', () => {
+  let nuxt
+  const buildDir = resolve(__dirname, '.nuxt-pro')
+
+  beforeAll(async () => {
+    const config = {
+      rootDir: resolve(__dirname, '..'),
+      buildDir,
+      srcDir: resolve(__dirname, '../example'),
+      modules: [
+        ['@@', {
+          component: 'fa',
+          useLayers: false,
+          useLayersText: false,
+          proIcons: {
+            solid: true,
+            duotone: ['faCamera']
+          }
+        }]
+      ]
+    }
+    config.dev = false
+
+    nuxt = new Nuxt(config)
+    const BundleBuilder = { build: _ => _ }
+    const builder = new Builder(nuxt, BundleBuilder)
+    await builder.build()
+  }, 60000)
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+
+  test('plugin', async () => {
+    const content = await fs.readFile(resolve(buildDir, 'fontawesome.js'), { encoding: 'utf8' })
+
+    expect(content).toContain('proFasFas')
+    expect(content).toContain('proFadFaCamera')
+    expect(content).toContain('pro-solid-svg-icons')
+    expect(content).toContain('pro-duotone-svg-icons')
+
+    expect(content).not.toContain('free-solid-svg-icons')
+    expect(content).not.toContain('FontAwesomeLayers')
+    expect(content).not.toContain('FontAwesomeLayersText')
+  })
+})
